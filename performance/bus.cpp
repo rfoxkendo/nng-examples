1da577010f1db17e9fe93e8b9803acf12a474c98
Ron Fox <fox@frib.msu.edu>
2025-01-14 11:59:05 -0500

    *  Rewrite the bus measurements to try to get good shutdown and completion...still some don't. *  Update the Makefile to turn off optimization explicitly. *  Get data for bus performance.

--- performance/bus.cpp
+++ performance/bus.cpp
@@ -29,12 +29,46 @@
  * 
  * Etc.  With TCP transport, best to keep the bus size less than 10.
  * 
- * Bus sends are not reliable so we need to figure out how to stop.  Furthermore,
- * std::thread does not hava tryjoin so we can't just send messages until 
- * threads exit.
+ *Bus sends don't deliver reliably.  Therefore we do this as follows:
+ *The first uint32_t of each message is a sequence number.  
+ * The receiving threads are packaged tasks and run until they see a sequence number
+ * >= the nmsg value.  Then they exit with just some garbage integer value -- for now.
+ * The main thread sets up the receiver threads and puts itself on the bus at location
+ * 0.  It then sends messages until all of the futures for the packaged tasks are
+ * valid - at which point it joins the threads and for the heck of it collects thair
+ * values.
+ * 
+ * Note for task/future newbies like me;  The return from a packaged task makes its
+ * future valid by setting its return value to the retun avalue of the function.
+ * 
+ * The part that is timed is the sending of messages, and final joining of the 
+ * threads.
+ * 
+ * @todo In the future we can have the tasks count the number of missed messages,
+ * collect them in the main thread and output the loss statistics as well as timings.
+ * 
+ * @todo Even the above is not reliable because (I think) the bus teardown needs
+ * to be carefully controlled (or done after all is done).  What we may want to do 
+ * is not use packaged tasks but copy in a future to a regular thread.
+ * 
+ * 1. The future is set when timing should be done.
+ * 2. After which the software loops on receiving a termination msg.
+ *    which e.g. has a seq of 0xffffffff
+ * 3. The main thread, after timing is done does another sleep to
+ *   ensure the system is pretty idle and sends the termination
+ *   message and when the termination message is rceived,
+ * 4.The receiver thread closes down and finishes.
+ * 5.After sending the termination message, the main thread
+ *   joins the receiver threads.
+ * 
+ * As long as the terminate message can be received by every receiver,
+ * I think this will be reliable and I _think_ the sleep ensures that it
+ * will be received since everyone should be in a condition to receiveit.
  */
 
+#include <future>
 #include <thread>
+#include <functional>
 #include <nng/nng.h>
 #include <nng/protocol/bus0/bus.h>
 
@@ -147,16 +181,16 @@ setupBus(std::vector<std::string> uris, int me, nng_socket s) {
 /**
  * receiver
  *    This function is a receiver (not bus 0 thread).
- *   It receives the specified number of messges then exits.
+ *   It receives messages until the sequence number is at least nmsg.
  * 
  * @param base - base URI.
  * @param me   - My position on the bus.
  * @param size - Size of bus.
  * @param nmsg - number of messages that will be received.
  * 
- * 
+ * @return int - SEQ # of last message received.
  */
-static void
+static int
 receiver(std::string base, size_t size, int me, size_t nmsg) {
     nng_socket s;
     void*  pMsg;
@@ -174,11 +208,15 @@ receiver(std::string base, size_t size, int me, size_t nmsg) {
     // Recieve nmsg bus messages using zero copy 
     // then exit:
 
-    for (int i = 0; i < nmsg; i++ ) {
+    uint32_t  lastseq = 0;
+    
+    while (lastseq < nmsg) {
         checkstat(
             nng_recv(s, &pMsg, &msgSize, NNG_FLAG_ALLOC),
             "Unable to receive a message from the bus."
         );
+        uint32_t* pSeq = reinterpret_cast<uint32_t*>(pMsg);
+        lastseq = *pSeq;
         nng_free(pMsg, msgSize);
     }
 
@@ -186,7 +224,11 @@ receiver(std::string base, size_t size, int me, size_t nmsg) {
         nng_close(s),
         "Failed to close receiver socket."
     );
+
     // Done.
+
+    return lastseq;
+
 }
 
 
@@ -213,10 +255,17 @@ int main(int argc, char** argv) {
     // we need to start the other threads (receivers) before we can
     // setup our bus access since 
     // setupBus blocks waiting for everyone else.
+    // For each receiver, we create a task, get and save its future in futures
+    // and start the task in a thread which we also save in receivers.
+
 
     std::vector<std::thread*> receivers;
+    std::vector<std::packaged_task<int(std::string, size_t, int, size_t)>*> tasks;
+    std::vector<std::future<int>*> futures;
     for (int i = 1; i < busSize; i++) {  // 1 since we (0) are not threads.
-        receivers.push_back( new std::thread(receiver, baseUri, busSize, i, nmsg));
+        tasks.push_back(new std::packaged_task<int(std::string, size_t, int, size_t)>(&receiver));
+        futures.push_back(new std::future<int>(tasks[tasks.size()-1]->get_future()));
+        receivers.push_back( new std::thread(std::move(*tasks[tasks.size()-1]), baseUri, busSize, i, nmsg));
     }
     // Now we can do our part to complete the bus:
 
@@ -227,29 +276,35 @@ int main(int argc, char** argv) {
     uint8_t* message = new uint8_t[msgSize];
 
     auto start  = std::chrono::high_resolution_clock::now();
-    for (int i =0; i < nmsg; i++ ) {
+    int done = 0;         // _count_ of the done tasks. 
+    int seq = 0;
+    while (done != tasks.size()) {
+        uint32_t* msgseq = reinterpret_cast<uint32_t*>(message);
+        *msgseq = seq++;
         checkstat(
             nng_send(s, message, msgSize, 0),
             "Failed to send message on the bus"
         );
-    }
-    // since there's no reliable delivery, we send 20% more at 1 byte each _sigh_
+        // Count the done tasks.
 
-    auto extra = nmsg/20;
-    for (int i = 0; i < extra; i++) {
-        checkstat(
-            nng_send(s, message, 1, 0),
-            "Failed to send extra msg."
-        );
+        done = 0;
+        for (auto p : futures) {
+            auto zero = start-start;    // Zero.
+            // Hopefully this is a non-blocking poll
+            if (p->wait_for(zero) == std::future_status::ready) done++; 
+        }
     }
-
+    // All futures being ready means all tasks/threads are done:
     // Done timing when all the threads exited -- so they got all the msgs
 
+    for (auto p : futures) {
+        p->get();
+    }
     for (auto p : receivers) {
         p->join();
     }
     auto end = std::chrono::high_resolution_clock::now();
-
+    std::cerr << "timing done\n";
 
     // Release resources:
 
@@ -270,8 +325,8 @@ int main(int argc, char** argv) {
     auto duration = end - start;
     
     double timing = (double)std::chrono::duration_cast<std::chrono::milliseconds>(duration).count()/1000.0;
-    double msgTiming = (double)nmsg/timing;
-    double xferTiming = (double)(nmsg * msgSize)/timing;
+    double msgTiming = (double)seq/timing;
+    double xferTiming = (double)(seq * msgSize)/timing;   /// Use actual message count.
 
     std::cout << "Time:       " << timing << std::endl;
     std::cout << "msgs/sec:   " << msgTiming << std::endl;
