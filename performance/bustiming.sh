b30b0faca5cb0c1caa7b4e77bd27088dbaede97a
Ron Fox <fox@frib.msu.edu>
2025-01-15 08:49:03 -0500

    Add a script to do bus timings collecting outputs in bus.log -- should probably have done this for all the timers.

---
+++ performance/bustiming.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script to gather all the timing for  the bus:
+
+echo Bus timing log > bus.log
+
+for service in tcp://localhost:300%d ipc:///tmp/bus%d inproc://bus%d
+do      
+	       echo ----    $service timings -------- >> bus.log
+
+for size in 1024 2048 4096 8192 16384 32768 65536 131072 262144 54288 1048576
+do
+    for members in 2 3 4 5
+    do
+	echo msg size $size members $members >>bus.log
+	./bus tcp://localhost:300%d 10000 $size $members >> bus.log
+    done
+done
+done
+		   
