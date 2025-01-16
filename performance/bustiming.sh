31d246056ca8aca378c81affb97073032b4ac354
Ron Fox <fox@frib.msu.edu>
2025-01-16 08:03:52 -0500

    Incorporate updated bus timings.

--- performance/bustiming.sh
+++ performance/bustiming.sh
@@ -8,7 +8,7 @@ for service in tcp://localhost:300%d ipc:///tmp/bus%d inproc://bus%d
 do      
 	       echo ----    $service timings -------- >> bus.log
 
-for size in 1024 2048 4096 8192 16384 32768 65536 131072 262144 54288 1048576
+for size in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576
 do
     for members in 2 3 4 5
     do
